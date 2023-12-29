f2f7e8361d0892da52688f2446a45cbaf94ac390
masat7353 <masat7353@gmail.com>
2023-12-29 07:35:42 +0000

    フォーマッター確認のために実装

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,18 @@
 <template>
-  <div>
-    aaa
+  <div :value="AAA" class="aaa" @click="name()">
+    aaaa
   </div>
 </template>
 
-<script setup>
-console.log("aaa")
+<script setup lang="ts">
+const AAA = ref("aaa")
+function name() {
+  console.log("aaa")
+}
 </script>
+
+<style>
+.aaa {
+  background-color: aqua;
+}
+</style>
